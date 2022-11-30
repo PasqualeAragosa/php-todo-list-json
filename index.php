0672b95ec2f7c7563da0c4eb176a4eded686407f
Pasquale Aragosa <aragosap@gmail.com>
2022-11-30 17:06:08 +0100

    add style

--- index.php
+++ index.php
@@ -6,17 +6,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
 </head>
 
 <body>
 
     <div id='app'>
-        <h1>TODO LIST</h1>
-        <ul>
-            <li v-for="todo in todoElements">{{todo}}</li>
-        </ul>
+        <div class="wrapper w-75 m-auto mt-5">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-3">
+                        <h1>TODO LIST</h1>
+                        <ul class="list-group mt-4">
+                            <li v-for="todo in todoElements" class="list-group-item">{{todo}}</li>
+                        </ul>
+                    </div>
+                    <!-- /.col-6 -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container -->
+        </div>
+        <!-- /.wrapper -->
     </div>
 
+    <!-- Script -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.1.3/axios.min.js" integrity="sha512-0qU9M9jfqPw6FKkPafM3gy2CBAvUWnYVOfNPDYKVuRTel1PrciTj+a9P3loJB+j0QmN2Y0JYQmkBBS8W+mbezg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <script src="./app.js"></script>
