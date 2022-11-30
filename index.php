a45ea290deb94545a87c3270dfaf340a48e24a27
Pasquale Aragosa <aragosap@gmail.com>
2022-11-30 16:47:53 +0100

    add elements

--- index.php
+++ index.php
@@ -10,6 +10,17 @@
 
 <body>
 
+    <div id='app'>
+        <h1>TODO LIST</h1>
+        <ul>
+            <li v-for="todo in todoElements">{{todo}}</li>
+        </ul>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.1.3/axios.min.js" integrity="sha512-0qU9M9jfqPw6FKkPafM3gy2CBAvUWnYVOfNPDYKVuRTel1PrciTj+a9P3loJB+j0QmN2Y0JYQmkBBS8W+mbezg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="./app.js"></script>
+
 </body>
 
 </html>
